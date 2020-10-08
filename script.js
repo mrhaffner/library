35e40fc930726f2e79eeb0aef58d2f7d87836087
Matt Haffner <mrhaffner13@gmail.com>
2020-10-08 11:53:50 -0500

    added working toggle buttons

--- script.js
+++ script.js
@@ -28,18 +28,22 @@ function addBookToLibrary() {
     let pages = document.getElementById('pages').value;
     let read = document.getElementById('read').value;
     //document.getElementById('book_form').submit()
-    console.log(title, author, pages, read)
     myLibrary.push(new Book(title, author, pages, read))
     document.getElementById('book_form').reset()
-    return false
+    displayBooks()
+    
+    readBtn.value = 'Finished Reading';
+    addReadToggle()
+    //return false
 }
 
 const submitBtn = document.getElementById('submit')
 submitBtn.addEventListener('click', () => addBookToLibrary())
 
 
-
 //optional: create a validation function
+
+const bookContainer = document.getElementById('book-container')
 
 function displayBooks() {
     // loops through the array and displays each book on the page
@@ -50,18 +54,47 @@ function displayBooks() {
             <p>${book.title}</p>
             <p>${book.author}</p>
             <p>${book.pages}</p>
-            <p>${book.read}</p>
-            <button>Completed/Finished</button>
+            <input type='button' class='read' value='Finished Reading'></input>
             <button>Delete</button>
             </div>`
         bookContainer.innerHTML = htmlElements;
     }); 
 }
 
-const bookContainer = document.getElementById('book-container')
+function addReadToggle() {
+    let toggle = document.querySelectorAll('.read')
+    toggle.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.value === 'Finished Reading') {
+                button.value = 'Have Not Finished';
+            } else {
+                button.value = 'Finished Reading';
+            };
+        })
+    })
+}
 
+const readBtn = document.getElementById('read')
+readBtn.addEventListener('click', () => {
+    if (readBtn.value === 'Finished Reading') {
+        readBtn.value = 'Have Not Finished';
+    } else {
+        readBtn.value = 'Finished Reading';
+    };
+})
+
+function clickToggleEvent (button) {
+    button.addEventListener('click', () => {
+        if (button.value === 'Finished Reading') {
+            button.value = 'Have Not Finished';
+        } else {
+            button.value = 'Finished Reading';
+        };
+    })
+}
 
 //add a "NEW BOOK" button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it's been read...
+    //hidden from at the button that pops up
 
 //add a button on each book's diplay to remove the book from the library
     //will need to associate your DOM elements with the actual book objext in some way
@@ -71,3 +104,6 @@ const bookContainer = document.getElementById('book-container')
     //create the function that toggles a book's "read" status on your "book" prototype
 
 
+//Remove this once fully operational, just for testing
+displayBooks()
+addReadToggle()
