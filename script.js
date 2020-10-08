d1267c60607bb5cd608720b46ff0a6b6d664270a
Matt Haffner <mrhaffner13@gmail.com>
2020-10-08 14:07:14 -0500

    added functional delete buttons, now need to make them remove associate book object from array

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ function addBookToLibrary() {
     myLibrary.push(new Book(title, author, pages, read))
     document.getElementById('book_form').reset()
     displayBooks()
-    
     readBtn.value = 'Finished Reading';
     addReadToggle()
+    addDelete()
     //return false
 }
 
@@ -55,7 +55,7 @@ function displayBooks() {
             <p>${book.author}</p>
             <p>${book.pages}</p>
             <input type='button' class='read' value='${book.read}'></input>
-            <button>Delete</button>
+            <button class='delete'>Delete</button>
             </div>`
         bookContainer.innerHTML = htmlElements;
     }); 
@@ -90,28 +90,30 @@ function updateReadStatus(button) {
     }
 }
 
-//if it's parent's elements class === card
-    //get the id of the parent element
-        //change the read status of the object in array at place = id to button.value
-
-
-
-//function clickUpdateReadStatus (book) {
-//    if (book. === 'Finished Reading') {
-//        button.value = 'Have Not Finished';
-//    } else {
-//        button.value = 'Finished Reading';
-//    };
-//}
-
-//add a "NEW BOOK" button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it's been read...
-    //hidden from at the button that pops up
-
 //add a button on each book's diplay to remove the book from the library
     //will need to associate your DOM elements with the actual book objext in some way
         //easy solution is giving them a data-attribute that corresponds to the index of the library array
 
 
-//Remove this once fully operational, just for testing
+
+function deleteBook(button) {
+    button.parentElement.remove()
+}
+
+function addDelete() {
+    let deleteBtn = document.querySelectorAll('.delete');
+    deleteBtn.forEach((button) => {
+        button.addEventListener('click', () => deleteBook(button))
+    });
+}
+
+
+
+//add a "NEW BOOK" button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it's been read...
+    //hidden from at the button that pops up
+
+
+//Remove this once fully operational, just for testing - also delete your test book objects
 displayBooks()
 addReadToggle()
+addDelete()
